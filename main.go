5be1240a067cd68890cdd9f450b4d681260e344e
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2021-07-03 17:18:17 +0900

    ログ一覧表示

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"sort"
 	"text/template"
+	"time"
 )
 
 var (
@@ -21,15 +25,46 @@ func getenv(key string, defaultValue string) string {
 	return ret
 }
 
+type Trace struct {
+	ID     string `json:"id"`
+	ExecAt time.Time
+}
+
+func checkTraceID(v string) bool {
+	rep := regexp.MustCompile(`^[0-9]{8}-[0-9]{6}$`)
+	return rep.MatchString(v)
+}
+
+func getTraces() []Trace {
+	files, err := ioutil.ReadDir("./logs")
+	if err != nil {
+		panic(err)
+	}
+	traceList := []Trace{}
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	for _, file := range files {
+		if checkTraceID(file.Name()) {
+			traceID := file.Name()
+			t, _ := time.Parse("20060102-150405", traceID)
+			jt := t.In(jst)
+			trace := Trace{ID: traceID, ExecAt: jt}
+			traceList = append(traceList, trace)
+		}
+	}
+	sort.Slice(traceList, func(i int, j int) bool { return traceList[i].ID > traceList[j].ID })
+	return traceList
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/home.html")
 	if err != nil {
 		log.Printf("%s\n", err.Error())
 		return
 	}
+	traces := getTraces()
 	w.WriteHeader(200)
 	w.Header().Set("Content-type", "text/html")
-	if t.Execute(w, map[string]interface{}{}) != nil {
+	if t.Execute(w, map[string]interface{}{"traces": traces}) != nil {
 		log.Printf("%s\n", err.Error())
 	}
 }
