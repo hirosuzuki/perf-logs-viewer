8588aeef9a0a5386c1c84d0fe1b57f61d6041b5b
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2021-07-03 15:36:20 +0900

    環境変数から設定をロードする

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
@@ -16,7 +17,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "perf-logs-viewer (%s)", revision)
 }
 
+func getenv(key string, defaultValue string) string {
+	ret := os.Getenv(key)
+	if ret == "" {
+		return defaultValue
+	}
+	return ret
+}
+
 func main() {
+	// Load Settings
+	perflogs_port := getenv("PERFLOGS_PORT", ":8080")
+	perflogs_dir := getenv("PERFLOGS_DIRN", "logs")
+	// Log Start Message
+	log.Printf("Start Perf-Logs-Viewer (port=%s, dir=%s)\n", perflogs_port, perflogs_dir)
+	// Routing Settings
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Start Web App Server
+	log.Fatal(http.ListenAndServe(perflogs_port, nil))
 }
