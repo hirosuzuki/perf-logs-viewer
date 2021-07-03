78238326f825751c9f61a91319527d3bc586847f
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2021-07-03 17:03:48 +0900

    Bulma テンプレートを導入

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 var (
@@ -12,10 +12,6 @@ var (
 	revision string
 	build    string
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "perf-logs-viewer (%s)", revision)
-}
 
 func getenv(key string, defaultValue string) string {
 	ret := os.Getenv(key)
@@ -25,6 +21,19 @@ func getenv(key string, defaultValue string) string {
 	return ret
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		log.Printf("%s\n", err.Error())
+		return
+	}
+	w.WriteHeader(200)
+	w.Header().Set("Content-type", "text/html")
+	if t.Execute(w, map[string]interface{}{}) != nil {
+		log.Printf("%s\n", err.Error())
+	}
+}
+
 func main() {
 	// Load Settings
 	perflogs_port := getenv("PERFLOGS_PORT", ":8080")
@@ -32,7 +41,10 @@ func main() {
 	// Log Start Message
 	log.Printf("Start Perf-Logs-Viewer (port=%s, dir=%s)\n", perflogs_port, perflogs_dir)
 	// Routing Settings
-	http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/favicon.ico", fs)
+	http.HandleFunc("/", homeHandler)
 	// Start Web App Server
 	log.Fatal(http.ListenAndServe(perflogs_port, nil))
 }
