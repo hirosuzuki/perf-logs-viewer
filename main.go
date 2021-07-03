85be0693df8caf01fe3bcef72064694e33eb7fc7
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2021-07-04 00:44:41 +0900

    詳細画面の途中

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -28,11 +30,19 @@ func getenv(key string, defaultValue string) string {
 	return ret
 }
 
+type LogFile struct {
+	Code     string
+	Filename string
+	Size     int64
+}
+
 type Trace struct {
-	ID            string `json:"id"`
-	ExecAt        time.Time
-	AccessLogSize int64
-	SQLLogSize    int64
+	ID             string `json:"id"`
+	ExecAt         time.Time
+	AccessLogs     []LogFile
+	AccessLogTotal int64
+	SQLLogs        []LogFile
+	SQLLogTolal    int64
 }
 
 func checkTraceID(v string) bool {
@@ -40,17 +50,54 @@ func checkTraceID(v string) bool {
 	return rep.MatchString(v)
 }
 
-func getFileSize(filename string) int64 {
-	file, err := os.Open(filename)
-	if err != nil {
-		return 0
+func makeLogFile(logfile fs.FileInfo, prefix string) (LogFile, bool) {
+	logfileName := logfile.Name()
+	if strings.HasPrefix(logfileName, prefix) && strings.HasSuffix(logfileName, ".log") {
+		filesize := logfile.Size()
+		var code string = logfileName[len(prefix) : len(logfileName)-len(".log")]
+		return LogFile{Code: code, Filename: logfile.Name(), Size: filesize}, true
 	}
-	defer file.Close()
-	stat, err := file.Stat()
-	if err != nil {
-		return 0
+	return LogFile{}, false
+}
+
+func getTrace(file fs.FileInfo) (Trace, bool) {
+	if !checkTraceID(file.Name()) {
+		return Trace{}, false
 	}
-	return stat.Size()
+
+	traceID := file.Name()
+	t, _ := time.Parse("20060102-150405", traceID)
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	jt := t.In(jst)
+	accessLogs := []LogFile{}
+	var accessLogTotal int64
+	sqlLogs := []LogFile{}
+	var sqlLogTotal int64
+	logfiles, err := ioutil.ReadDir(filepath.Join(perflogs_dir, traceID))
+	if err != nil {
+		panic(err)
+	}
+	for _, logfile := range logfiles {
+		if accessLog, ok := makeLogFile(logfile, "access"); ok {
+			accessLogs = append(accessLogs, accessLog)
+			accessLogTotal += accessLog.Size
+		}
+		if sqlLog, ok := makeLogFile(logfile, "sql"); ok {
+			sqlLogs = append(sqlLogs, sqlLog)
+			sqlLogTotal += sqlLog.Size
+		}
+	}
+
+	trace := Trace{
+		ID:             traceID,
+		ExecAt:         jt,
+		AccessLogTotal: accessLogTotal,
+		AccessLogs:     accessLogs,
+		SQLLogTolal:    sqlLogTotal,
+		SQLLogs:        sqlLogs,
+	}
+
+	return trace, true
 }
 
 func getTraces() []Trace {
@@ -59,18 +106,8 @@ func getTraces() []Trace {
 		panic(err)
 	}
 	traceList := []Trace{}
-	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 	for _, file := range files {
-		if checkTraceID(file.Name()) {
-			traceID := file.Name()
-			t, _ := time.Parse("20060102-150405", traceID)
-			jt := t.In(jst)
-			trace := Trace{
-				ID:            traceID,
-				ExecAt:        jt,
-				AccessLogSize: getFileSize(filepath.Join(perflogs_dir, traceID, "access.log")),
-				SQLLogSize:    getFileSize(filepath.Join(perflogs_dir, traceID, "sql.log")),
-			}
+		if trace, ok := getTrace(file); ok {
 			traceList = append(traceList, trace)
 		}
 	}
@@ -102,6 +139,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func detailHandler(w http.ResponseWriter, r *http.Request) {
+	tradeID := r.URL.Path[8:]
+	filename := filepath.Join(perflogs_dir, tradeID)
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+
+	}
+	if fs, err := f.Stat(); err == nil {
+		trace, ok := getTrace(fs)
+		log.Printf("%v %v\n", ok, trace)
+	}
+	w.WriteHeader(200)
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprintf(w, "OK %s", tradeID)
+}
+
 var (
 	perflogs_port string
 	perflogs_dir  string
@@ -117,6 +171,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/favicon.ico", fs)
+	http.HandleFunc("/detail/", detailHandler)
 	http.HandleFunc("/", homeHandler)
 	// Start Web App Server
 	log.Fatal(http.ListenAndServe(perflogs_port, nil))
